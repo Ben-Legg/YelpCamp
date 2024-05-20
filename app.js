073925fa05c87d1fc8ef4f34aab5c3194815fc1b
Ben-Legg <benlegg2002@gmail.com>
2024-05-21 00:22:00 +0100

    Added db edit functionality

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
+const methodOverride = require("method-override");
 const Campground = require("./models/campground"); // import model
 
 // connect to db
@@ -19,6 +20,7 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.use(express.urlencoded({extended: true})); // parse incoming incoming request payloads for accessing submitted form body
+app.use(methodOverride("_method")); // enable support for HTTP PUT and DELETE
 
 app.get("/", (req, res) => {
     res.render("home");
@@ -35,19 +37,31 @@ app.get("/campgrounds/new", async (req, res) => {
     res.render("campgrounds/new");
  });
  
- app.post("/campgrounds", async (req, res) => {
-     const campground = new Campground(req.body.campground);
-     await campground.save();
-     res.redirect(`/campgrounds/${campground._id}`);
-  });
+// route for submitting new campground
+app.post("/campgrounds", async (req, res) => {
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
+});
 
 // route for campgrounds detail page
 app.get("/campgrounds/:id", async (req, res) => {
-    const campground = await Campground.findById(req.params.id)
+    const campground = await Campground.findById(req.params.id);
     res.render("campgrounds/show", { campground });
  });
 
+// route for editing campgrounds
+ app.get("/campgrounds/:id/edit", async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    res.render("campgrounds/edit", { campground });
+ });
 
+ // route for submitting updated campground details
+ app.put("/campgrounds/:id", async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground }); // spread posted object into new object to update db
+    res.redirect(`/campgrounds/${campground._id}`);
+ });
 
 app.listen(3000, () => {
     console.log("Serving on port 3000");
