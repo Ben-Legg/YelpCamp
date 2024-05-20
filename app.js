3e795ad3f7340f7a5ab2d9e4ab7e924b5db902a7
Ben-Legg <benlegg2002@gmail.com>
2024-05-20 18:17:06 +0100

    Added db seeding functionality

--- app.js
+++ app.js
@@ -1,13 +1,33 @@
 const express = require("express");
-const app = express();
 const path = require("path");
+const mongoose = require("mongoose");
+const Campground = require("./models/campground"); // import model
 
+// connect to db
+mongoose.connect("mongodb://127.0.0.1:27017/yelp-camp");
+
+// logic to check for errors
+mongoose.connection.on("error", console.error.bind(console, "connection error:"));
+mongoose.connection.once("open", () => {
+    console.log("Database connected");
+});
+
+const app = express();
+
+// set view engine and define relative path to views
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.get("/", (req, res) => {
     res.render("home");
 })
+
+app.get("/makecampground", async (req, res) => {
+    const camp = new Campground({ title: "My Backyard", description: "Cheap Camping !" })
+    await camp.save();
+    res.send(camp);
+})
+
 
 app.listen(3000, () => {
     console.log("Serving on port 3000");
---
+++ seeds/index.js
@@ -0,0 +1,38 @@
+// self contained file used to seed db with campground locations
+
+
+const mongoose = require("mongoose");
+const cities = require("./cities"); // import cities array
+const {places, descriptors} = require("./seed-helpers"); // import model
+const Campground = require("../models/campground"); // import model
+
+// connect to db
+mongoose.connect("mongodb://127.0.0.1:27017/yelp-camp");
+
+// logic to check for errors
+mongoose.connection.on("error", console.error.bind(console, "connection error:"));
+mongoose.connection.once("open", () => {
+    console.log("Database connected");
+});
+
+const randomCampName = array => array[Math.floor(Math.random() * array.length)]; // pick a random value from array to create unique camp descriptions
+
+// function to seed db with random campground data 
+async function seedDB() {
+    await Campground.deleteMany({}); // delete existing documents in database collection
+    for (let i = 0; i < 50; i++){ // create 50 random instances of Campground model using data from cities array
+        const randomCityIndex = Math.floor(Math.random() * 1000);
+        const camp = new Campground({
+            location: `${cities[randomCityIndex].city}, ${cities[randomCityIndex].state}`,
+            title: `${randomCampName(descriptors)} ${randomCampName(places)}`
+        });
+        await camp.save();
+    }
+    console.log("Finished Seeding")
+}
+
+// close db connection
+seedDB().then(() => {
+    mongoose.connection.close();
+    console.log("Database closed");
+});
